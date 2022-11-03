b83d30417fa79666bba9681b1bb645fd3293ba91
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-11-03 22:09:08 +0800

    style: add an ;

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-const video = document.querySelector("video")
+const video = document.querySelector("video");
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.cmd == 'test') {
